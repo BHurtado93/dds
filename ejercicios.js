8173de07d81b4136cf75fe744f8efb798a7b0f79
BHurtado93 <b.hurtado93@est.tecnologicovalparaiso.cl>
2022-09-22 01:56:25 -0300

    ejercicio dds

---
+++ ejercicios.js
@@ -0,0 +1,5 @@
+const misJugadores = ["Juan Martin", "Gabriel", "Leonardo", "Brayan", "Alexander", "Agustin", "Vicente"];
+
+for (let i = 0; i < misJugadores.length; i++){
+    console.log(misJugadores[i]);
+}
